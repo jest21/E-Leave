6f615c6e20a9759235f013d8956ed3fd506b697e
livin <livinmathew99@gmail.com>
2017-03-23 18:50:41 +0530

    User Session Persistance

--- app/src/main/java/com/example/user/e_leave/Login.java
+++ app/src/main/java/com/example/user/e_leave/Login.java
@@ -2,13 +2,22 @@ package com.example.user.e_leave;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login extends AppCompatActivity {
     EditText edt_Username, edt_Password;
+
+    private FirebaseAuth mAuth;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,22 @@ public class Login extends AppCompatActivity {
         edt_Password = (EditText) findViewById(R.id.edt_Password);
         System.out.println("Hai");
         Button btlogin = (Button) findViewById(R.id.btlogin);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("Login", "onAuthStateChanged:signed_in:" + user.getUid());
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
         btlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
