7cadec3ac6ac289773fa9ba8bf4d976d330bfd16
livin <livinmathew99@gmail.com>
2017-03-26 11:17:51 +0530

    Redesigned Login Activity

--- app/src/main/java/com/example/user/e_leave/Login.java
+++ app/src/main/java/com/example/user/e_leave/Login.java
@@ -4,6 +4,8 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -17,21 +19,32 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.regex.Pattern;
+
 public class Login extends AppCompatActivity {
-    EditText edt_Username, edt_Password;
+    TextInputEditText edt_Username, edt_Password;
+    TextInputLayout emailInputLayout, passInputLayout;
 
     private FirebaseAuth mAuth;
 
     private FirebaseAuth.AuthStateListener mAuthListener;
 
+
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        edt_Username = (EditText) findViewById(R.id.edt_Username);
-        edt_Password = (EditText) findViewById(R.id.edt_Password);
-        System.out.println("Hai");
-        Button btlogin = (Button) findViewById(R.id.btlogin);
+        edt_Username = (TextInputEditText) findViewById(R.id.edt_Username);
+        edt_Password = (TextInputEditText) findViewById(R.id.edt_Password);
+        emailInputLayout = (TextInputLayout) findViewById(R.id.emailInputLayout);
+        passInputLayout = (TextInputLayout) findViewById(R.id.passwordInputLayout);
+
+        Button loginButton = (Button) findViewById(R.id.btlogin);
 
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -48,15 +61,16 @@ public class Login extends AppCompatActivity {
             }
         };
 
-        btlogin.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 validation(edt_Username.getText().toString().trim(), edt_Password.getText().toString().trim());
 
             }
         });
-        Button btnewuser = (Button) findViewById(R.id.btnewuser);
-        btnewuser.setOnClickListener(new View.OnClickListener() {
+
+        Button newUserButton = (Button) findViewById(R.id.btnewuser);
+        newUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Login.this, Registration.class);
@@ -68,32 +82,46 @@ public class Login extends AppCompatActivity {
 
     private void validation(String id, String pass) {
 
-        final ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Please Wait...");
-        progressDialog.setCancelable(false);
-        progressDialog.show();
+        emailInputLayout.setErrorEnabled(false);
+        passInputLayout.setErrorEnabled(false);
+        emailInputLayout.setErrorEnabled(true);
+        passInputLayout.setErrorEnabled(true);
 
-        mAuth.signInWithEmailAndPassword(id, pass)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d("Login", "signInWithEmail:onComplete:" + task.isSuccessful());
+        if (id.isEmpty()){
+            emailInputLayout.setError("Please Enter Your Email ID");
+        }else if (pass.isEmpty()){
+            passInputLayout.setError("Please Enter Your Password");
+        }else if (!Pattern.matches(EMAIL_PATTERN,id)){
+            emailInputLayout.setError("Invalid Email ID");
+        }else {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Please Wait...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
 
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w("Login", "signInWithEmail:failed", task.getException());
-                            Toast.makeText(Login.this, "Login Failed",
-                                    Toast.LENGTH_SHORT).show();
-                        }else {
-                            Intent intent = new Intent(Login.this, MainActivity.class);
-                            startActivity(intent);
-                            finish();
+            mAuth.signInWithEmailAndPassword(id, pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d("Login", "signInWithEmail:onComplete:" + task.isSuccessful());
+                            if (!task.isSuccessful()) {
+                                Log.w("Login", "signInWithEmail:failed", task.getException());
+                                if (task.getException().toString().toLowerCase().contains("no user record")){
+                                    emailInputLayout.setError("Not a registered user");
+                                }else if (task.getException().toString().toLowerCase().contains("password is invalid")){
+                                    passInputLayout.setError("Wrong Password");
+                                }
+                            }else {
+                                Intent intent = new Intent(Login.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                            progressDialog.dismiss();
                         }
-                        progressDialog.dismiss();
-                    }
-                });
+                    });
+        }
+
+
     }
 
     @Override
@@ -108,6 +136,10 @@ public class Login extends AppCompatActivity {
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
+
+    public void forgotPassword(View view) {
+        startActivity(new Intent(this,ForgotPassword.class));
     }
 }
 
