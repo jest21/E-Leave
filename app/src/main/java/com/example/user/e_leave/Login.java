0337ed30b5814182791c36b9919746a6045d2d39
livin <livinmathew99@gmail.com>
2017-03-26 11:51:41 +0530

    Registration Design

--- app/src/main/java/com/example/user/e_leave/Login.java
+++ app/src/main/java/com/example/user/e_leave/Login.java
@@ -10,8 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
--- app/src/main/java/com/example/user/e_leave/Registration.java
+++ app/src/main/java/com/example/user/e_leave/Registration.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -18,13 +19,20 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.regex.Pattern;
+
 /***
  * Created by User on 24-01-2017.
  */
 public class Registration extends AppCompatActivity {
     EditText edt_facualtyId, edt_Name, edt_Dept, edt_Designation, edt_Phone, edt_Emailid, edt_Password;
-
+    TextInputLayout fIdLayout, nameLayout, deptLayout, desigLayout, phoneLayout, emailLayout, passLayout;
     FirebaseAuth auth;
+
+
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +45,13 @@ public class Registration extends AppCompatActivity {
         edt_Phone = (EditText) findViewById(R.id.edt_Phone);
         edt_Emailid = (EditText) findViewById(R.id.edt_Emailid);
         edt_Password = (EditText) findViewById(R.id.edt_Password);
+        fIdLayout = (TextInputLayout) findViewById(R.id.facultyIdInputLayout);
+        nameLayout = (TextInputLayout) findViewById(R.id.nameInputLayout);
+        deptLayout = (TextInputLayout) findViewById(R.id.deptInputLayout);
+        desigLayout = (TextInputLayout) findViewById(R.id.designationInputLayout);
+        phoneLayout = (TextInputLayout) findViewById(R.id.phoneNumberInputLayout);
+        emailLayout = (TextInputLayout) findViewById(R.id.emailInputLayout);
+        passLayout = (TextInputLayout) findViewById(R.id.passwordInputLayout);
         Button btregister = (Button) findViewById(R.id.btregister);
 
         auth = FirebaseAuth.getInstance();
@@ -63,30 +78,39 @@ public class Registration extends AppCompatActivity {
     }
 
     private void Validation(final String facultyId, final String name, final String dept, final String desgn, final String phone, final String email, String password) {
-        if (facultyId.length() == 0 || name.length() == 0 || dept.length() == 0) {
-            if (facultyId.length() == 0) {
-                edt_facualtyId.setError("Field can't be blank.");
-            }
-            if (name.length() == 0) {
-                edt_Name.setError("Field can't be blank.");
-            }
-            if (dept.length() == 0) {
-                edt_Dept.setError("field cant be blank");
-            }
-            if (desgn.length() == 0) {
-                edt_Designation.setError("field cant be blank");
-            }
 
-            if (phone.length() == 0) {
-                edt_Phone.setError("field cant be blank");
-            }
+        fIdLayout.setErrorEnabled(false);
+        nameLayout.setErrorEnabled(false);
+        desigLayout.setErrorEnabled(false);
+        deptLayout.setErrorEnabled(false);
+        emailLayout.setErrorEnabled(false);
+        passLayout.setErrorEnabled(false);
+        phoneLayout.setErrorEnabled(false);
+        fIdLayout.setErrorEnabled(true);
+        nameLayout.setErrorEnabled(true);
+        desigLayout.setErrorEnabled(true);
+        deptLayout.setErrorEnabled(true);
+        emailLayout.setErrorEnabled(true);
+        passLayout.setErrorEnabled(true);
+        phoneLayout.setErrorEnabled(true);
 
-            if (email.length() == 0) {
-                edt_Emailid.setError("field cant be blank");
-            }
-
-
-        } else {
+        if (facultyId.length() == 0) {
+            fIdLayout.setError("Required Field");
+        }else if (name.length() == 0) {
+            nameLayout.setError("Required Field");
+        }else if (dept.length() == 0) {
+            deptLayout.setError("Required Field");
+        }else if (desgn.length() == 0) {
+            desigLayout.setError("Required Field");
+        }else if (phone.length() == 0) {
+            phoneLayout.setError("Required Field");
+        }else if (email.length() == 0) {
+            emailLayout.setError("Required Field");
+        }else if (password.length() == 0) {
+            passLayout.setError("Required Field");
+        }else if (!Pattern.matches(EMAIL_PATTERN,email)){
+            emailLayout.setError("Invalid Email Id");
+        }else {
             final ProgressDialog progressDialog = new ProgressDialog(this);
             progressDialog.setMessage("Registering...");
             progressDialog.setCancelable(false);
