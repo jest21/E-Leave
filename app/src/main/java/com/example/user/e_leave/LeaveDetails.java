482df675356925c5c2dc5f54bab7b610e8b085cf
livin <livinmathew99@gmail.com>
2017-03-26 23:43:25 +0530

    Leave Details Redesign 2

--- app/src/main/java/com/example/user/e_leave/LeaveDetails.java
+++ app/src/main/java/com/example/user/e_leave/LeaveDetails.java
@@ -13,30 +13,51 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.Calendar;
-
 public class LeaveDetails extends AppCompatActivity {
 
-    TextView leavesRemainingTextView, leavesTakenTextView;
-    int halfDayLeaves = 0, fullDayLeaves = 0;
+    TextView sickLeavesRemainingTextView,
+            casualLeavesRemainingTextView,
+            sickLeavesTakenTextView,
+            casualLeavesTakenTextView,
+            maternityLeavesTakenTextView,
+            otherLeavesTakenTextView;
+
+    int halfSickLeavesCount = 0;
+    int halfCasualLeavesCount = 0;
+    int halfMaternityLeavesCount = 0;
+    int halfOtherLeavesCount = 0;
+    int fullSickLeavesCount = 0;
+    int fullCasualLeavesCount = 0;
+    int fullMaternityLeavesCount = 0;
+    int fullOtherLeavesCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leavedetails);
+
         Button btdetailsok= (Button) findViewById(R.id.btdetailsok);
-        leavesRemainingTextView = (TextView) findViewById(R.id.leavesRemainingTextView);
-        leavesTakenTextView = (TextView) findViewById(R.id.leavesTakenTextView);
+
+        sickLeavesRemainingTextView = (TextView) findViewById(R.id.remainingSickLeavesTextView);
+        casualLeavesRemainingTextView = (TextView) findViewById(R.id.remainingCasualLeaves);
+
+        sickLeavesTakenTextView = (TextView) findViewById(R.id.sickLeavesTextView);
+        casualLeavesTakenTextView = (TextView) findViewById(R.id.casualLeavesTaken);
+        maternityLeavesTakenTextView = (TextView) findViewById(R.id.maternityLeavesTaken);
+        otherLeavesTakenTextView = (TextView) findViewById(R.id.otherLeavesTaken);
+
         btdetailsok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
         final ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Fetching Details...");
         progressDialog.setCancelable(false);
         progressDialog.show();
+
         FirebaseDatabase.getInstance().getReference().child("leave_applications")
                 .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                 .child(new Months().getNameOfThisMonth())
@@ -44,38 +65,63 @@ public class LeaveDetails extends AppCompatActivity {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         for (DataSnapshot child : dataSnapshot.getChildren()){
+
                             String fromDateStr = child.child("from").getValue().toString();
                             String toDateStr = child.child("to").getValue().toString();
-                            Calendar fromDate = Calendar.getInstance();
-                            fromDate.set(
-                                    Integer.parseInt(fromDateStr.split("-")[2]),
-                                    Integer.parseInt(fromDateStr.split("-")[1]),
-                                    Integer.parseInt(fromDateStr.split("-")[0])
-                            );
-                            Calendar toDate = Calendar.getInstance();
-                            toDate.set(
-                                    Integer.parseInt(toDateStr.split("-")[2]),
-                                    Integer.parseInt(toDateStr.split("-")[1]),
-                                    Integer.parseInt(toDateStr.split("-")[0])
-                            );
 
-                            if (!child.child("type").getValue().toString().toLowerCase().equals("maternity")){
+                            int to = Integer.parseInt(toDateStr.split("-")[0]);
+                            int from = Integer.parseInt(fromDateStr.split("-")[0]);
+
+                            int count = to - from + 1;
+
+                            if (child.child("type").getValue().toString().toLowerCase().equals("maternity")){
                                 if (child.child("days_type").getValue().toString().toLowerCase().equals("half")){
-                                    halfDayLeaves += (toDate.get(Calendar.DAY_OF_MONTH) - fromDate.get(Calendar.DAY_OF_MONTH));
-                                    halfDayLeaves += 1;
+                                    halfMaternityLeavesCount += count;
                                 }else {
-                                    fullDayLeaves += (toDate.get(Calendar.DAY_OF_MONTH) - fromDate.get(Calendar.DAY_OF_MONTH));
-                                    fullDayLeaves += 1;
+                                    fullMaternityLeavesCount += count;
+                                }
+                            }else if (child.child("type").getValue().toString().toLowerCase().equals("sick")){
+                                if (child.child("days_type").getValue().toString().toLowerCase().equals("half")){
+                                    halfSickLeavesCount += count;
+                                }else {
+                                    fullSickLeavesCount += count;
+                                }
+                            }else if (child.child("type").getValue().toString().toLowerCase().equals("casual")){
+                                if (child.child("days_type").getValue().toString().toLowerCase().equals("half")){
+                                    halfCasualLeavesCount += count;
+                                }else {
+                                    fullCasualLeavesCount += count;
+                                }
+                            }else {
+                                if (child.child("days_type").getValue().toString().toLowerCase().equals("half")){
+                                    halfOtherLeavesCount += count;
+                                }else {
+                                    fullOtherLeavesCount += count;
                                 }
                             }
                         }
-                        double totalNoOfLeaves = (halfDayLeaves/2.0) + fullDayLeaves;
-                        leavesTakenTextView.setText(String.valueOf(totalNoOfLeaves) + " days");
-                        if (totalNoOfLeaves > 0){
-                            leavesRemainingTextView.setText("0 days");
+                        double totalNoOfSickLeaves = (halfSickLeavesCount /2.0) + fullSickLeavesCount;
+                        double totalNoOfMaternityLeaves = (halfMaternityLeavesCount /2.0) + fullMaternityLeavesCount;
+                        double totalNoOfCasualLeaves = (halfCasualLeavesCount /2.0) + fullCasualLeavesCount;
+                        double totalNoOfOtherLeaves = (halfOtherLeavesCount /2.0) + fullOtherLeavesCount;
+
+                        sickLeavesTakenTextView.setText(String.valueOf(totalNoOfSickLeaves) + " days");
+                        maternityLeavesTakenTextView.setText(String.valueOf(totalNoOfMaternityLeaves) + " days");
+                        casualLeavesTakenTextView.setText(String.valueOf(totalNoOfCasualLeaves) + " days");
+                        otherLeavesTakenTextView.setText(String.valueOf(totalNoOfOtherLeaves) + " days");
+
+                        if (totalNoOfCasualLeaves > 0){
+                            casualLeavesRemainingTextView.setText("0 days");
                         }else {
-                            leavesRemainingTextView.setText("1 day");
+                            casualLeavesRemainingTextView.setText("1 day");
                         }
+
+                        if (totalNoOfSickLeaves > 0){
+                            sickLeavesRemainingTextView.setText("0 days");
+                        }else {
+                            sickLeavesRemainingTextView.setText("1 day");
+                        }
+
                         progressDialog.dismiss();
                     }
 
